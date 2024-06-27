4bf3c4ed10af48030a79f432f0e5921b296b5c01
Mirko Gutierrez <mirko.gutierrez.dev@gmail.com>
2024-06-27 17:24:32 -0400

    ultinos cambios a la pagina de solicitud

--- src/components/SolicitudesPage.jsx
+++ src/components/SolicitudesPage.jsx
@@ -1,35 +1,81 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 import { differenceInDays, parseISO } from 'date-fns';
 import AdmSolView from "./AdmSolView";
 import FeriadoSolView from "./FeriadoSolView";
+import {getDiasWork} from "../services/services"
 
 function SolicitudesPage({ data }) {
+
+    const currentYear = new Date().getFullYear();
+
     const adm = data ? data.diasAdm : [];
     const feriados = data ? data.feriados : [];
 
     const [option, setOption] = useState('');
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
+    const [diasWork,setDiasWork] = useState(0);
 
+     // Filtra los feriados para mostrar solo los del año actual
+     const filteredFeriados = feriados.filter(feriado => {
+        const feriadoYear = feriado.anio; // Asumiendo que cada feriado tiene un campo 'date'
+
+        return feriadoYear === currentYear;
+    });
+    
+
+    // Extrae los datos necesarios del primer objeto filtrado (asumiendo que hay solo uno por año)
+    const {
+        
+        diasPendientes
+    } = filteredFeriados.length > 0 ? filteredFeriados[0] : { totalDias: 0, diasTomados: 0, diasPendientes: 0 };
+
+ 
+
+    
     const handleOptionChange = (e) => {
         setOption(e.target.value);
-        console.log("Option selected:", e.target.value);
+        //console.log("Option selected:", e.target.value);
     };
 
     const handleStartDateChange = (e) => {
         setStartDate(e.target.value);
-        console.log("Start Date:", e.target.value);
+        //console.log("Start Date:", e.target.value);
         calculateDifference(e.target.value, endDate);
     };
 
     const handleEndDateChange = (e) => {
         setEndDate(e.target.value);
-        console.log("End Date:", e.target.value);
-        calculateDifference(startDate, e.target.value);
+    //    console.log("End Date:", e.target.value);
+       // calculateDifference(startDate, e.target.value);
+        
     };
+
+    const validaDias = async (fechaIni, fechaFin) => {
+
+        try {
+            const dias = await getDiasWork(fechaIni,fechaFin);
+
+            setDiasWork(dias);
+   
+           if(diasWork>diasPendientes){
+            alert("muchsa dias");
+           }
+            
+        } catch (error) {
+            console.log(error);
+            
+        }
+
+        
+
+        return null;
+    }
+
+
 
     const calculateDifference = (start, end) => {
         if (start && end) {
@@ -64,7 +110,7 @@ function SolicitudesPage({ data }) {
                     </Col>
                 </Row>
                 <Row>
-                    <Col md={4}>
+                    <Col md={3}>
                         <Form.Group controlId="formSelectOption" className="mt-3">
                             <Form.Label className="h5">Tipo de solicitud</Form.Label>
                             <Form.Control
@@ -80,7 +126,7 @@ function SolicitudesPage({ data }) {
                             </Form.Control>
                         </Form.Group>
                     </Col>
-                    <Col md={4}>
+                    <Col md={3}>
                         <Form.Group controlId="formStartDate" className="mt-3">
                             <Form.Label className="h5">Fecha de inicio</Form.Label>
                             <Form.Control
@@ -92,7 +138,7 @@ function SolicitudesPage({ data }) {
                             />
                         </Form.Group>
                     </Col>
-                    <Col md={4}>
+                    <Col md={3}>
                         <Form.Group controlId="formEndDate" className="mt-3">
                             <Form.Label className="h5">Fecha de término</Form.Label>
                             <Form.Control
@@ -103,6 +149,13 @@ function SolicitudesPage({ data }) {
                                 style={{ borderRadius: '5px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}
                             />
                         </Form.Group>
+                    </Col>
+                    <Col md={3} className="mt-3" >
+                   
+                            <Button className="mt-3"
+                            onClick={()=>validaDias(startDate,endDate)}
+                            >Validar</Button>
+                      
                     </Col>
                 </Row>
             </Container>
--- src/services/services.js
+++ src/services/services.js
@@ -42,3 +42,21 @@ export const getAusencias = async () => {
 }
 
 
+export const getDiasWork = async (fechaIni, fechaFin) => {
+    const url = `http://localhost:8081/api/calcular?fechaInicio=${fechaIni}&fechaFin=${fechaFin}`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        return result;
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
+};
+
+
