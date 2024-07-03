85222ad14e92fa093666938e05ba1bdfe8936b3c
Mirko Gutierrez <mirko.gutierrez.dev@gmail.com>
2024-07-03 17:06:44 -0400

    cambio api

--- src/services/services.js
+++ src/services/services.js
@@ -2,7 +2,7 @@
 
 export const getAll = async () => {
 
-    const url = `http://localhost:8080/api/buscar/10067570`;
+    const url = `http://localhost:8081/smc/buscar/10691840`;
 
     try {
         const response = await fetch(url);
