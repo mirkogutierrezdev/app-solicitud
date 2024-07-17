f8b851083d63c62e3531375f4eb467b47a8f6de6
Mirko Gutierrez <mirko.gutierrez.dev@gmail.com>
2024-07-17 17:24:25 -0400

    sigo trabajando en la ruta

--- src/components/SolicitudRow.jsx
+++ src/components/SolicitudRow.jsx
@@ -1,21 +1,22 @@
 /* eslint-disable react/prop-types */
 import { useContext, useEffect, useState } from "react";
 import { Button, Collapse, ListGroup } from "react-bootstrap";
-import { FaCircleCheck } from "react-icons/fa6";
-import { FaArrowAltCircleRight } from "react-icons/fa";
-import { FaCircleNotch } from "react-icons/fa";
 import { MdRemoveRedEye } from "react-icons/md";
 import { saveDerivacion, saveEntrada } from "../services/services";
 import DataContext from "../context/DataContext";
 import Swal from 'sweetalert2';
+import { FaCircleCheck, FaCircleNotch } from "react-icons/fa6";
+import { FaArrowAltCircleRight } from "react-icons/fa";
 
 const SolicitudRow = ({ solicitud }) => {
     const [open, setOpen] = useState(false);
     const infoFun = useContext(DataContext);
 
-    const [isRecibirDisabled, setRecibirDisabled] = useState(true);
     const [dataFunc, setDataFun] = useState({});
+    const { data } = dataFunc || {};
     const [dataDepartamento, setDataDepartamento] = useState({});
+    const [isRecibirDisabled, setRecibirDisabled] = useState(true);
+    const [isDerivarDisabled, setDerivarDisabled] = useState(true);
 
     useEffect(() => {
         if (infoFun && infoFun.data) {
@@ -29,7 +30,6 @@ const SolicitudRow = ({ solicitud }) => {
         const año = fechaActual.getFullYear();
         const mes = String(fechaActual.getMonth() + 1).padStart(2, '0');
         const dia = String(fechaActual.getDate()).padStart(2, '0');
-
         return `${año}-${mes}-${dia}`;
     };
 
@@ -41,7 +41,7 @@ const SolicitudRow = ({ solicitud }) => {
             idSolicitud: solicitud.solicitud.id,
             estado: "PENDIENTE",
             fechaDerivacion: fechaDerivacion,
-            rut: dataFunc.data.rut
+            rut: data.rut
         };
 
         try {
@@ -50,6 +50,8 @@ const SolicitudRow = ({ solicitud }) => {
                 text: "Derivación realizada con éxito",
                 icon: "success"
             });
+            setDerivarDisabled(true); // Deshabilitar el botón después de derivar con éxito
+            setRecibirDisabled(true); // Deshabilitar el botón "Recibir" después de derivar con éxito
         } catch (error) {
             Swal.fire({
                 text: "Error al grabar la derivación",
@@ -64,7 +66,7 @@ const SolicitudRow = ({ solicitud }) => {
         const entrada = {
             solicitudId: solicitud.solicitud.id,
             fechaEntrada: fechaEntrada,
-            rut: dataFunc.data.rut
+            rut: data ? data.rut : null
         };
 
         try {
@@ -74,6 +76,7 @@ const SolicitudRow = ({ solicitud }) => {
                 icon: "success"
             });
             setRecibirDisabled(true); // Deshabilitar el botón después de recibir con éxito
+            setDerivarDisabled(false); // Habilitar el botón "Derivar" después de recibir con éxito
         } catch (error) {
             Swal.fire({
                 text: "Error al grabar la Recepción",
@@ -87,29 +90,46 @@ const SolicitudRow = ({ solicitud }) => {
         // Lógica para rechazar la solicitud
     };
 
+    const verificarEstadoBotones = () => {
+        const derivacionesSinEntrada = solicitud?.derivaciones?.filter(derivacion => {
+            return derivacion.departamento.deptoSmc == dataDepartamento.depto && 
+                !solicitud.entradas.some(entrada => entrada.derivacion.id === derivacion.id);
+        });
+
+        
+        const derivacionesConEntrada = solicitud?.derivaciones?.filter(derivacion => {
+            return derivacion.departamento.deptoSmc == dataDepartamento.depto && 
+                solicitud.entradas.some(entrada => entrada.derivacion.id === derivacion.id);
+        });
+
+        //console.log(solicitud);
+        console.log(derivacionesSinEntrada);
+        
+
+        if (derivacionesSinEntrada?.length > 0) {
+            
+            setRecibirDisabled(false);
+        
+        } else {
+            setRecibirDisabled(true);
+        }
+
+        if (derivacionesConEntrada?.length > 0) {
+            setDerivarDisabled(false);
+        } else {
+            setDerivarDisabled(true);
+        }
+    };
+
     useEffect(() => {
-        const verificarEstadoBotonRecibir = () => {
-            const lastDerivacion = solicitud?.derivaciones?.length > 0
-                ? solicitud.derivaciones[solicitud.derivaciones.length - 1]
-                : null;
-
-            const hasEntrada = solicitud?.entradas?.some(entrada => entrada.solicitudId === solicitud.solicitud.id);
-
-            // Logs para depuración
-            console.log("Última derivación:", lastDerivacion);
-            console.log("Entradas:", solicitud?.entradas);
-            console.log("Departamento actual:", dataDepartamento.depto);
-            console.log("¿Tiene entrada?:", hasEntrada);
-
-            if (lastDerivacion && lastDerivacion.departamento.deptoSmc === dataDepartamento.depto && !hasEntrada) {
-                setRecibirDisabled(false);
-            } else {
-                setRecibirDisabled(true);
-            }
-        };
-
-        verificarEstadoBotonRecibir();
+        verificarEstadoBotones();
     }, [solicitud, dataDepartamento]);
+
+    
+
+    const isCurrentDepartment = solicitud?.derivaciones?.some(derivacion => 
+        derivacion.departamento.deptoSmc == dataDepartamento.depto
+    );
 
     return (
         <>
@@ -128,12 +148,14 @@ const SolicitudRow = ({ solicitud }) => {
                     <Button
                         variant="warning"
                         onClick={handleGuardarYDerivar}
+                        disabled={isDerivarDisabled}
                     >
                         Derivar <FaArrowAltCircleRight />
                     </Button>{" "}
                     <Button
                         variant="danger"
                         onClick={handleRechazar}
+                        disabled={!isCurrentDepartment}
                     >
                         Rechazar <FaCircleNotch />
                     </Button>
